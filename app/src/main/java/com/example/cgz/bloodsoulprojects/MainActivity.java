75ce1e7695f87edd0ebb9eb987a9781a3f340848
caiguangzu <caiguangzu@gionee.com>
2017-11-29 18:32:18 +0800

    first commit

---
+++ app/src/main/java/com/example/cgz/bloodsoulprojects/MainActivity.java
@@ -0,0 +1,200 @@
+package com.example.cgz.bloodsoulprojects;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.cgz.bloodsoulprojects.nfc.NFCActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> mDatas = new ArrayList<>();
+
+    private RecyclerView mRecyclerView;
+
+    private RecyclerAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+        initView();
+    }
+
+    private void initData() {
+        mDatas.add("num - title - sub");
+        mDatas.add("1 - NFC读卡器, 仿真卡");
+    }
+
+    private void clickRecyclerItem(int position) {
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+                startActivity(NFCActivity.class);
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            case 10:
+                break;
+            case 11:
+                break;
+            case 12:
+                break;
+            case 13:
+                break;
+            case 14:
+                break;
+            case 15:
+                break;
+            case 16:
+                break;
+            case 17:
+                break;
+            case 18:
+                break;
+            case 19:
+                break;
+            case 20:
+                break;
+            case 21:
+                break;
+            case 22:
+                break;
+            case 23:
+                break;
+            case 24:
+                break;
+            case 25:
+                break;
+            case 26:
+                break;
+            case 27:
+                break;
+            case 28:
+                break;
+            case 29:
+                break;
+            case 30:
+                break;
+            case 31:
+                break;
+            case 32:
+                break;
+            case 33:
+                break;
+            case 34:
+                break;
+            case 35:
+                break;
+            case 36:
+                break;
+            case 37:
+                break;
+            case 38:
+                break;
+            case 39:
+                break;
+            case 40:
+                break;
+            case 41:
+                break;
+            case 42:
+                break;
+            case 43:
+                break;
+            case 44:
+                break;
+            case 45:
+                break;
+            case 46:
+                break;
+            case 47:
+                break;
+            case 48:
+                break;
+            case 49:
+                break;
+        }
+    }
+
+    private void initView() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mAdapter = new RecyclerAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setHasFixedSize(true);
+    }
+
+    private class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.RecyclerHolder> {
+
+        @Override
+        public RecyclerHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.recycler_item, null);
+            return new RecyclerHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerHolder holder, int position) {
+            holder.mTextView.setText(mDatas.get(position));
+            final int clickPosition = holder.getAdapterPosition();
+            holder.mTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    clickRecyclerItem(clickPosition);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            if (mDatas == null) {
+                return 0;
+            }
+            return mDatas.size();
+        }
+
+        class RecyclerHolder extends RecyclerView.ViewHolder {
+
+            TextView mTextView;
+
+            public RecyclerHolder(View itemView) {
+                super(itemView);
+                mTextView = (TextView) itemView.findViewById(R.id.tv);
+            }
+        }
+
+    }
+
+    private void startActivity(Class clazz) {
+        startActivity(new Intent(this, clazz));
+    }
+
+}
+
---
+++ app/src/main/java/com/example/cgz/bloodsoulprojects/MainApplication.java
@@ -0,0 +1,12 @@
+package com.example.cgz.bloodsoulprojects;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
